10e549b7df185133d34eacb4c258f5affcfddd3b
Leonardo Ledda <leonardoledda@gmail.com>
2024-01-03 15:27:00 +0100

    sepolicy: lineage -> bootleg
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -5,8 +5,8 @@
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/bootleg/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/bootleg/sepolicy/atv/private
